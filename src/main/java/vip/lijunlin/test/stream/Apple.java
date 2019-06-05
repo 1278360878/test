1b598db4f1e8d695a2cf0a1b0033407afa0fb3e7
lijunlin <851025486@qq.com>
2019-06-05 18:01:22 +0800

    2019-06-05

--- src/main/java/vip/lijunlin/test/stream/Apple.java
+++ src/main/java/vip/lijunlin/test/stream/Apple.java
@@ -3,4 +3,33 @@ package vip.lijunlin.test.stream;
 public class Apple {
 
 	private int weight;// 重量
+
+	private String color;// 颜色
+
+	public Apple(int weight, String color) {
+		super();
+		this.weight = weight;
+		this.color = color;
+	}
+
+	public Apple() {
+		super();
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
 }
--- src/main/java/vip/lijunlin/test/stream/MyStreamTest.java
+++ src/main/java/vip/lijunlin/test/stream/MyStreamTest.java
@@ -1,5 +1,27 @@
 package vip.lijunlin.test.stream;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyStreamTest {
 
+	public static void main(String[] args) {
+		List<Apple> apples = new ArrayList<Apple>();
+		apples.add(new Apple(100, "red"));
+		apples.add(new Apple(101, "green"));
+		apples.add(new Apple(103, "green"));
+		// TODO
+		List<Apple> heavyApples1 = apples.stream().filter((Apple a) -> a.getWeight() > 100).collect(null);
+		System.out.println(heavyApples1);
+	}
+
+	public static List<Apple> filterGreenApples(List<Apple> inventory) {
+		List<Apple> result = new ArrayList<Apple>();
+		for (Apple apple : inventory) {
+			if ("green".equals(apple.getColor())) {
+				result.add(apple);
+			}
+		}
+		return result;
+	}
 }
